e135a7014333611ce3711692d179136189e8e8a3
Pochana Srikar Reddy <pochasri@amazon.com>
2024-12-22 14:06:02 +0530

    Loading effect and Resize Observer fix

---
+++ leetcode-visualizer/src/components/Loading.jsx
@@ -0,0 +1,14 @@
+import '../styles/Loading.css';
+
+export const LoadingComponent = () => {
+    return (
+        <div className="editor-loading">
+            <div className="loading-ring">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    );
+};
--- leetcode-visualizer/src/page/AlgoVisualizer.jsx
+++ leetcode-visualizer/src/page/AlgoVisualizer.jsx
@@ -1,17 +1,20 @@
 import React, { useState } from 'react';
 import CodeEditor from '../components/CodeEditor';
 import '../styles/AlgoVisualizer.css';
+import {LoadingComponent} from "../components/Loading";
 
 const AlgoVisualizer = () => {
     const [output, setOutput] = useState('');
     const [error, setError] = useState('');
     const [input, setInput] = useState('');
     const [code, setCode] = useState('');
+    const [isRunning, setIsRunning] = useState(false);
     const [language, setLanguage] = useState('python');
 
     const executeCode = async () => {
         try {
-            const response = await fetch('https://algoviz-backend-lkg5.onrender.com/api/python/execute', {
+            setIsRunning(true);
+            const response = await fetch('http://localhost:5123/api/python/execute', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ code, input }),
@@ -19,6 +22,7 @@ const AlgoVisualizer = () => {
             const data = await response.json();
             setOutput(data.output || '');
             setError(data.error || '');
+            setIsRunning(false);
         } catch (err) {
             console.error('Error executing code:', err);
         }
@@ -51,12 +55,15 @@ const AlgoVisualizer = () => {
                     />
                 </div>
                 <div className="output-section">
-                    <div className='wrapper'>
-                        <h2>Output</h2>
-                        <button className="run-button" onClick={executeCode}>Run Code</button>
-                    </div>
-                    {output && <pre>Output: {output}</pre>}
-                    {error && <pre>Error: {error}</pre>}
+                    {isRunning ? <LoadingComponent/>
+                        : (
+                        <div className='wrapper'>
+                            <h2>Output</h2>
+                            <button className="run-button" onClick={executeCode}>Run Code</button>
+                        </div>
+                        )}
+                    {!isRunning && output && <pre>{output}</pre>}
+                    {!isRunning && error && <pre>Error: {error}</pre>}
                 </div>
             </div>
         </div>
